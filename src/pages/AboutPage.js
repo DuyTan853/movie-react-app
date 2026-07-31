e15f5a2ab39cbfc17a5307fbd1638e36cd362ef4
duytan853 <nguyenduytan.08052003@gmail.com>
2026-07-31 08:25:27 +0700

    Create About Page

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,19 @@
+function AboutPage() {
+    return (
+      <div>
+        <h1>About Team</h1>
+  
+        <p>Movie App using React Router.</p>
+  
+        <h3>Members</h3>
+  
+        <ul>
+          <li>Nguyễn Duy Tân</li>
+          <li>Member 2</li>
+          <li>Member 3</li>
+        </ul>
+      </div>
+    );
+  }
+  
+  export default AboutPage;
