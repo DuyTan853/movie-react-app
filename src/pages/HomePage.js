171637fbf138686bc8e1e41e2dc42f167ebc66e5
duytan853 <nguyenduytan.08052003@gmail.com>
2026-07-31 08:25:04 +0700

    Create Home Page

---
+++ src/pages/HomePage.js
@@ -0,0 +1,38 @@
+const movies = [
+    {
+      id: 1,
+      title: "Avengers Endgame",
+      image: "https://picsum.photos/200/300?1",
+      desc: "Marvel movie"
+    },
+    {
+      id: 2,
+      title: "Spider Man",
+      image: "https://picsum.photos/200/300?2",
+      desc: "Peter Parker"
+    },
+    {
+      id: 3,
+      title: "Batman",
+      image: "https://picsum.photos/200/300?3",
+      desc: "DC movie"
+    }
+  ];
+  
+  function HomePage() {
+    return (
+      <div>
+        <h1>Movie List</h1>
+  
+        {movies.map((movie) => (
+          <div key={movie.id}>
+            <img src={movie.image} width="150" alt={movie.title} />
+            <h3>{movie.title}</h3>
+            <p>{movie.desc}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+  
+  export default HomePage;
