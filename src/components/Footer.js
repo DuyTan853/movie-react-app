4cc1ace71f8122ae47835ddafbdab638e690aded
duytan853 <nguyenduytan.08052003@gmail.com>
2026-07-31 08:24:36 +0700

    Add Header and Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+function Footer() {
+    return (
+      <footer
+        style={{
+          marginTop: 30,
+          padding: 20,
+          textAlign: "center",
+          background: "#333",
+          color: "#fff"
+        }}
+      >
+        © 2026 Movie App
+      </footer>
+    );
+  }
+  
+  export default Footer;
